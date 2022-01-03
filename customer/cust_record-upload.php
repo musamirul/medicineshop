742c2696944f0dec2939561bf9eeb2874a9221b0
ameirul mustaqim <musamirul.kpj@gmail.com>
2022-01-04 01:18:39 +0800

    update

--- customer/cust_record-upload.php
+++ customer/cust_record-upload.php
@@ -7,45 +7,41 @@
     }
     $Cust_Id = $_SESSION['Cust_Id'];
 ?>
+
 <form enctype="multipart/form-data" method="post">
     Select File
     <input type="file" name="file"/>
-    <input type="text" name="fileName" placeholder="Enter File Type" />
     <button type="submit" name="submit">Submit</button>
 </form>
-
 
 <table>
     <thead>
         <tr>
-            <th>Type</th>
             <th>Files</th>
             <th>Action</th>
-        </thead>
+        </tr>
+    </thead>
 <?php
-    $query_showDoc = mysqli_query($con,"SELECT * FROM declaration WHERE FK_Declaration_Cust_ID = '$Cust_Id'");
+    $query_showDoc = mysqli_query($con,"SELECT * FROM record WHERE FK_Record_Cust_ID = '$Cust_Id'");
     while($result_showDoc = mysqli_fetch_array($query_showDoc)){
-        $name = $result_showDoc['Declaration_FileName'];   
+        $name = $result_showDoc['Record_FileName'];   
 ?>
     <tbody>
         <tr>
-            <td><?php echo $result_showDoc['Declaration_Name']?></td>
             <td><?php echo $name; ?></td>
-            <td><a href="cust_declaration-download.php?filename=<?php echo $name;?>&f=<?php echo $result_showDoc['Declaration_File']?>"><button>Download</button></a></td>
+            <td><a href="cust_declaration-download.php?filename=<?php echo $name;?>&f=<?php echo $result_showDoc['Record_File']?>"><button>Download</button></a></td>
         </tr>
     </tbody>
 <?php
     }
 ?>
 </table>
-
 <?php
 if(isset($_POST['submit'])){
     $name=$_FILES['file']['name'];
     $size=$_FILES['file']['size'];
     $type=$_FILES['file']['type'];
     $temp=$_FILES['file']['tmp_name'];
-    $fileName = $_POST['fileName'];
 
 
     //get date and time
@@ -55,10 +51,10 @@ if(isset($_POST['submit'])){
     $fname = date("YmdHis").'_'.$name;
     $move = move_uploaded_file($temp,"upload/".$fname);
     
-    $query_uploadFile=mysqli_query($con,"INSERT INTO declaration(Declaration_Name, Declaration_FileName, Declaration_File, Declaration_TimeStamp, FK_Declaration_Cust_ID) 
-    VALUES ('$fileName','$name','$fname','$date','$Cust_Id')");
+    $query_uploadFile=mysqli_query($con,"INSERT INTO record(Record_Timestamp, Record_File, Record_FileName, FK_Record_Product_ID, FK_Record_Cust_ID)
+    VALUES ('$date','$fname','$name','0','$Cust_Id')");
 
-    header("Location:cust_declaration-upload.php?msg=success");
+    header("Location:cust_record-upload.php?msg=success");
     
 }
 ?>
