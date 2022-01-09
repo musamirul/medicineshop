25142e0be73521821b59ac92f0a11b0e49c15f00
ameirul mustaqim <musamirul.kpj@gmail.com>
2022-01-10 00:09:49 +0800

    update seller product view

--- seller/Seller_Product-View.php
+++ seller/Seller_Product-View.php
@@ -60,11 +60,62 @@
                 <i class="bi bi-list-ul"></i>
               </button>
               <ul class="dropdown-menu" aria-labelledby="actionButton">
-                <li><a class="dropdown-item" href="#"><i class="bi bi-pencil-square"></i> Edit</a></li>
+                <li><a class="dropdown-item" href="#"><button type="button" data-bs-toggle="modal" data-bs-target="#editModal<?php echo $id ?>"><i class="bi bi-pencil-square"></i> Edit</a></button></li>
                 <li><a class="dropdown-item" href="#"><button type="button" data-bs-toggle="modal" data-bs-target="#deleteModal<?php echo $id ?>"><i class="bi bi-x-square-fill"></i> Delete</a></button></li> 
               </ul>
             </td>
           </tr>
+
+      <!-- Edit Modal -->
+      <div class="modal fade" id="editModal<?php echo $id?>" tabindex="-1" aria-labelledby="editModalLabel" class="modal fade" role="dialog">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="editModalLabel">Edit <strong><?php echo $name?></strong> ?</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+                <form method="post">
+                <div class="modal-body">
+                  <div class="form-group row">
+                    
+                    <div class="col-sm-12">
+                    <label>Name</label>
+                        <input class="form-control" placeholder="Enter Name" name="nameEdit" value="<?php echo $result_image['Product_Name']; ?>" required autofocus="autofocus" />
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    
+                    <div class="col-sm-12">
+                    <label>Description</label>
+                        <textarea class="form-control" rows="5" cols="50" name="descriptionEdit"><?php echo $result_image['Product_Desc']; ?></textarea>
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    
+                    <div class="col-sm-4">
+                    <label>Quantity</label>
+                    <input class="form-control" placeholder="Enter Quantity" name="quantityEdit" value="<?php echo $result_image['Product_Qty']; ?>" required autofocus="autofocus" />
+                    </div>
+                    <div class="col-sm-4">
+                    <label>Price</label>
+                    <input class="form-control" placeholder="Enter Price" name="priceEdit" value="<?php echo $result_image['Product_SellingPrice']; ?>" required autofocus="autofocus" />
+                    </div>
+                    <div class="col-sm-4">
+                    <label>Manufacturer</label>
+                    <input class="form-control" placeholder="Enter Manufacturer" name="manufacturerEdit" value="<?php echo $result_image['Product_ManufacturerName']; ?>" required autofocus="autofocus" />
+                    </div>
+                  </div>
+                </div>
+                 
+                <div class="modal-footer">
+                  <input type="hidden" value="<?php echo $result_image['Product_ID']?>" name="idEdit" />
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="submit" name="editProduct" class="btn btn-primary">Edit</button>
+                </form>
+                </div>   
+            </div>
+          </div>
+        </div>
 
       <!-- Delete Modal -->
        <div class="modal fade" id="deleteModal<?php echo $id?>" tabindex="-1" aria-labelledby="deleteModalLabel" class="modal fade" role="dialog">
@@ -87,6 +138,23 @@
             </div>
           </div>
         </div>
+      
+      <!-- php Edit Modal script -->
+      <?php
+        if(isset($_POST['editProduct'])){
+          $nameEdit = $_POST['nameEdit'];
+          $descriptionEdit = $_POST['descriptionEdit'];
+          $quantityEdit = $_POST['quantityEdit'];
+          $priceEdit = $_POST['priceEdit'];
+          $manufacturerEdit = $_POST['manufacturerEdit'];
+          $idEdit = $_POST['idEdit'];
+          
+          $editQuery = mysqli_query($con,"UPDATE product SET Product_Name='$nameEdit',Product_Desc='$descriptionEdit',Product_Qty='$quantityEdit',Product_ManufacturerName='$manufacturerEdit',
+          Product_SellingPrice='$priceEdit' WHERE Product_ID ='$idEdit'");
+          echo '<script>window.location.href="Seller_Product-View.php"</script>';
+
+        }
+      ?>
 
       <!-- php Delete Modal script-->
        <?php
