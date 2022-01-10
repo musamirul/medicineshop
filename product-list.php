2fff83320baf35849a17448b39a42ba2e6bf2405
ameirul mustaqim <musamirul.kpj@gmail.com>
2022-01-11 00:22:41 +0800

    create product-list, product-view

--- product-list.php
+++ product-list.php
@@ -12,32 +12,53 @@ margin: 0 5px 0 0;
 }
 
 .imgProd{
-height: 60px;
+height: 100px;
 
 }
 </style>
-<div class="col-12">
-<form method="post">
-    <input class="form-control form-control-lg" type="text" name="productSearch" placeholder="Search for product, brand and shop">
-    <button type="submit" name="searchButton">Search</button>
-</form>
+<div class="container">
+    <div class="row">
+        <form method="post">
+            <div class="input-group mb-3">
+                <input class="form-control form-control-lg" type="text" name="productSearch" placeholder="Search for product, brand and shop">
+                <button type="submit" name="searchButton">Search</button>
+            </div>
+
+        </form>
+    </div>
 </div>
-<div class="col-12">
-    <?php
-        if($_POST['productSearch']==""){
-            $productSearch = 'acid';
-        }else{
-            $productSearch = $_POST['productSearch'];
-        }
-        $searchQuery = mysqli_query($con,"SELECT * FROM product
-        WHERE Product_Tags LIKE '%$productSearch%'");
-        
-        while($searchResult = mysqli_fetch_array($searchQuery))
-        {
-            echo '<p><img class="imgProd" src="seller/'.$searchResult['Product_Image'].'"></p>';
-            echo $searchResult['Product_Image'];
-        }
-    ?>
+
+<div class="col-12 mt-5">
+    <div class="container">
+        <div class="row">
+            <?php
+                if(isset($_POST['searchButton'])){
+                    $productSearch = $_POST['productSearch'];
+                    
+                    $searchQuery = mysqli_query($con,"SELECT * FROM product
+                    WHERE Product_Tags LIKE '%$productSearch%'");
+                    
+                    while($searchResult = mysqli_fetch_array($searchQuery))
+                    {
+            ?>
+            
+                <div class="col-2 mb-3">
+                    <a class="text-decoration-none text-reset" href="product-view.php?prodId=<?php echo $searchResult['Product_ID']?>">
+                        <div class="card" style="width: 11rem;">
+                            <img style="height: 10rem;" src="seller/<?php echo $searchResult['Product_Image']; ?>" class="card-img-top">
+                            <div class="card-body">
+                                <p class="card-title" style="font-size: 13px;"><?php echo $searchResult['Product_Name'] ?></p>
+                                <p class="card-text" style="font-size: 15px;color:red;">RM <?php echo $searchResult['Product_SellingPrice'] ?></p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            <?php            
+                    }
+                }
+            ?>
+        </div>
+    </div>
 </div>
 
 
---
+++ product-view.php
@@ -0,0 +1,55 @@
+<?php
+include("includes/config.php");
+include("Interface/header.php");
+//get id from link
+$id = intval($_GET['prodId']);
+?>
+
+<?php
+//Select product 
+$product_query = mysqli_query($con,"SELECT * FROM product WHERE Product_ID = '$id'");
+$product_result= mysqli_fetch_array($product_query);
+?>
+
+<div class="container bg-white shadow bg-body rounded">
+    <div class="row">
+        <div class="col-5 mt-3 mb-4">
+            <center><img style="height: 24rem;" src="seller/<?php echo $product_result['Product_Image']; ?>"></center>
+        </div>
+        <div class="col-7 mt-3 mb-5">
+            <div class="col">
+                <h5><?php echo $product_result['Product_Name'] ?></h5>
+            </div>
+            <div class="col p-2 bg-light">
+                <span class="text-danger fs-4"><b>RM<?php echo $product_result['Product_SellingPrice'] ?></b></span>
+            </div>
+            <div class="col">
+                <span>Shipping</span>
+            </div>
+            <div class="col">
+                <div class="row">
+                    <label class="col-sm-2 col-form-label">Quantity</label>
+                    <div class="col-sm-2">
+                        <input type="number" class="form-control" placeholder="Number">
+                    </div>
+                    <label class="col-sm-3 col-form-label"><?php echo $product_result['Product_Qty'] ?> Pieces Available</label>
+                </div>
+            </div>
+            <div class="col mt-3">
+                <button type="submit" class="btn btn-secondary">Add To Cart</button>
+                <button type="submit" class="btn btn-dark">Buy Now</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container mt-3 bg-white shadow bg-body rounded">
+    <p>Product Specification</p>
+</div>
+
+
+
+
+<?php
+include("Interface/footer.php")
+?>
