be7ca32d0dffe2f973f6f344d38f0d3657959448
musamirul <46254149+musamirul@users.noreply.github.com>
2022-02-22 17:35:51 +0800

    shipment

--- seller/Seller_Shipment.php
+++ seller/Seller_Shipment.php
@@ -107,6 +107,56 @@
                         <td><?php echo $result_tracking['Tracking_Status'] ?></td>
                         <td><?php echo $result_tracking['Tracking_Channel'] ?></td>
                     </tr>
+                        <!-- View Product list -->
+                        <div class="modal fade" id="productView<?php echo $Cart_ID ?>" tabindex="-1" aria-labelledby="editModalLabel" class="modal fade" role="dialog">
+                            <div class="modal-dialog modal-fullscreen">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h5 class="modal-title" id="editModalLabel">View Product Order <strong>#<?php echo $Order_ID ?></strong></h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="form-group row">
+                                    <div class="col-sm-12">
+                                        <div class="row m-2 p-2">
+                                            <div style="font-weight: bold;" class="col">Image</div>
+                                            <div style="font-weight: bold;" class="col">Name</div>
+                                            <div style="font-weight: bold;" class="col">Quantity</div>
+                                            <div style="font-weight: bold;" class="col">Price</div>
+                                        </div>
+                                        <span class="d-grid mx-auto mt-3 mb-3" style="border-bottom:0.5px solid rgb(241, 240, 240);"></span>
+                                        <?php 
+                                            $Cart_ID;
+                                            $Cust_ID;
+                                            $Seller_ID;
+
+                                            $query_cartItem = mysqli_query($con,"SELECT * FROM cart_item WHERE FK_Cart_ID ='$Cart_ID' AND FK_Item_Seller_ID='$Seller_ID'");
+                                            while($result_cartItem=mysqli_fetch_array($query_cartItem)){
+
+                                                $Product_ID = $result_cartItem['FK_Item_Product_ID'];
+                                                $query_product = mysqli_query($con,"SELECT * FROM product WHERE Product_ID = '$Product_ID'");
+                                                $result_product = mysqli_fetch_array($query_product);
+
+                                        ?>
+                                        <div class="row m-2 p-2">
+                                            <div class="col"><img style="height: 6rem; width:6rem;" src="../seller/<?php echo $result_product['Product_Image']; ?>"></div>
+                                            <div class="col mt-4"><?php echo $result_product['Product_Name'] ?></div>
+                                            <div class="col mt-4"><?php echo $result_cartItem['Cart_Item_Qty']?> qty</div>
+                                            <div class="col mt-4">RM <?php echo $result_cartItem['Cart_Item_Amount'] ?></div>
+                                        </div>
+                                        <span class="d-grid mx-auto" style="border-bottom:0.5px solid rgb(241, 240, 240);"></span>
+                                        <?php
+                                            }
+                                        ?>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </form>
+                                </div>   
+                            </div>
+                            </div>
+                        </div>
                     <?php
                         
                         }
@@ -117,55 +167,6 @@
     </div>
 </div>
 
-<!-- View Product list -->
-<div class="modal fade" id="productView<?php echo $Cart_ID ?>" tabindex="-1" aria-labelledby="editModalLabel" class="modal fade" role="dialog">
-    <div class="modal-dialog modal-fullscreen">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="editModalLabel">View Product Order <strong>#<?php echo $Order_ID ?></strong></h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-          <div class="modal-body">
-            <div class="form-group row">
-              <div class="col-sm-12">
-                <div class="row m-2 p-2">
-                    <div style="font-weight: bold;" class="col">Image</div>
-                    <div style="font-weight: bold;" class="col">Name</div>
-                    <div style="font-weight: bold;" class="col">Quantity</div>
-                    <div style="font-weight: bold;" class="col">Price</div>
-                </div>
-                <span class="d-grid mx-auto mt-3 mb-3" style="border-bottom:0.5px solid rgb(241, 240, 240);"></span>
-                <?php 
-                    $Cart_ID;
-                    $Cust_ID;
-                    $Seller_ID;
 
-                    $query_cartItem = mysqli_query($con,"SELECT * FROM cart_item WHERE FK_Cart_ID ='$Cart_ID' AND FK_Item_Seller_ID='$Seller_ID'");
-                    while($result_cartItem=mysqli_fetch_array($query_cartItem)){
-
-                        $Product_ID = $result_cartItem['FK_Item_Product_ID'];
-                        $query_product = mysqli_query($con,"SELECT * FROM product WHERE Product_ID = '$Product_ID'");
-                        $result_product = mysqli_fetch_array($query_product);
-
-                ?>
-                <div class="row m-2 p-2">
-                    <div class="col"><img style="height: 6rem; width:6rem;" src="../seller/<?php echo $result_product['Product_Image']; ?>"></div>
-                    <div class="col mt-4"><?php echo $result_product['Product_Name'] ?></div>
-                    <div class="col mt-4"><?php echo $result_cartItem['Cart_Item_Qty']?> qty</div>
-                    <div class="col mt-4">RM <?php echo $result_cartItem['Cart_Item_Amount'] ?></div>
-                </div>
-                <span class="d-grid mx-auto" style="border-bottom:0.5px solid rgb(241, 240, 240);"></span>
-                <?php
-                    }
-                ?>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          </form>
-          </div>   
-      </div>
-    </div>
-  </div>
 
 <?php include("Interface/footer.php"); ?>
